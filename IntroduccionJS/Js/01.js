e3dc3e7bfa51d61dfaa828e8b10b1338ff679e91
Matias Raffo <matiasraffo90@gmail.com>
2024-07-25 16:55:16 -0300

    ok

---
+++ IntroduccionJS/Js/01.js
@@ -0,0 +1,17 @@
+// Variables 
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // iniciamos la variable pero sin valor.
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible = true,
+    categoria = 'Computadoras';
+
+
+
+

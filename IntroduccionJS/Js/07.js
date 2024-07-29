25fcac08dcd2e17ef52dc0c98f96b35ddf4cd3aa
Matias Raffo <matiasraffo90@gmail.com>
2024-07-29 17:29:32 -0300

    ok

---
+++ IntroduccionJS/Js/07.js
@@ -0,0 +1,23 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;
+
+resultado = ( 100 + 200 + 300 ) * 1.16;
+
+//console.log(resultado);
+
+// incrementos
+
+let puntaje = 10;
+
+// incremento en 1 ++
+puntaje++; // 10
+++puntaje; // 12
+
+puntaje--;
+--puntaje;
+
+console.log (puntaje);
+
---
+++ IntroduccionJS/Js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'Matias';
+const email = 'correo@correo.com';
+
+// concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template Strings
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);

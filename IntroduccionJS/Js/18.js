d4a412d2e2598a6b02d6f225cf4c35f54cbd1610
Matias Raffo <matiasraffo90@gmail.com>
2024-09-17 17:42:27 -0300

    ok

--- IntroduccionJS/Js/18.js
+++ IntroduccionJS/Js/18.js
@@ -1,12 +1,14 @@
-function sumar() {
-    console.log(10 + 10);
+function sumar(numero1, numero2) { // numero1, 2 son parametros
+    console.log(numero1 + numero2);
 }
 
-sumar()
+sumar(10,10) // argumentos o valores reales
+sumar(60,10) // argumentos o valores reales
+sumar(7,5) // argumentos o valores reales
 
 
-const sumar2 = function() {
-    console.log(3 + 3);
+const sumar2 = function(n1, n2) {
+    console.log(n1 + n2);
 }
 
-sumar2()
+sumar2(5, 10)
